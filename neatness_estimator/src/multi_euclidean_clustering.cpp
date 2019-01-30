9e6d2add6174d440a62bc8387d489b0b4abdd429
taichiH <higashide@jsk.imi.i.u-tokyo.ac.jp>
2019-01-30 13:07:40 +0900

    add label infomation

--- neatness_estimator/src/multi_euclidean_clustering.cpp
+++ neatness_estimator/src/multi_euclidean_clustering.cpp
@@ -6,8 +6,11 @@
 #include <sensor_msgs/PointCloud2.h>
 #include <jsk_recognition_msgs/ClusterPointIndices.h>
 #include <jsk_recognition_msgs/BoundingBoxArray.h>
+#include <jsk_recognition_msgs/LabelArray.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl_msgs/PointIndices.h>
+#include <tf/transform_listener.h>
+#include <tf/transform_broadcaster.h>
 
 #include <pcl/point_types.h>
 #include <pcl/filters/extract_indices.h>
@@ -18,6 +21,7 @@
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
 #include <pcl/segmentation/extract_clusters.h>
+#include <pcl_ros/transforms.h>
 
 static pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
 static pcl::PointCloud<pcl::PointXYZ>::Ptr clustered_cloud(new pcl::PointCloud<pcl::PointXYZ>);
@@ -25,15 +29,36 @@ static pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_cloud(new pcl::PointCloud<pc
 static ros::Publisher output_cluster_indices_pub;
 static ros::Publisher output_box_pub;
 
+tf::TransformListener *listener_;
+
+struct Points {
+  Points() {
+    x = 0.0;
+    y = 0.0;
+    z = 0.0;
+  };
+  Points(float _x, float _y, float _z) {
+    x = _x;
+    y = _y;
+    z = _z;
+  };
+  float x, y, z;
+};
+
 void callback(const jsk_recognition_msgs::ClusterPointIndices::ConstPtr& cluster_indices,
-              const sensor_msgs::PointCloud2::ConstPtr& point_cloud)
+              const sensor_msgs::PointCloud2::ConstPtr& point_cloud,
+              const jsk_recognition_msgs::LabelArray::ConstPtr& labels)
 {
   cloud->clear();
   pcl::fromROSMsg(*point_cloud, *cloud);
+  // std::string target_frame_ = "map";
+  std::string target_frame_ = cluster_indices->header.frame_id;
 
   jsk_recognition_msgs::ClusterPointIndices output_cluster_indices;
   jsk_recognition_msgs::BoundingBoxArray output_box_msg;
+
   if(cloud->points.size() > 0){
+    int label_index = 0;
     for(auto point_indices : cluster_indices->cluster_indices){
       pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
       inliers->indices = point_indices.indices;
@@ -59,7 +84,7 @@ void callback(const jsk_recognition_msgs::ClusterPointIndices::ConstPtr& cluster
       tree->setInputCloud(filtered_cloud);
       std::vector<pcl::PointIndices> output_indices;
       pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
-      ec.setClusterTolerance (0.05); // 2cm
+      ec.setClusterTolerance (0.01);
       ec.setMinClusterSize (10);
       ec.setMaxClusterSize (5000);
       ec.setSearchMethod (tree);
@@ -67,6 +92,21 @@ void callback(const jsk_recognition_msgs::ClusterPointIndices::ConstPtr& cluster
       ec.setInputCloud (filtered_cloud);
       ec.extract (output_indices);
 
+      pcl::PointCloud<pcl::PointXYZ>::Ptr transformed_cloud (new pcl::PointCloud<pcl::PointXYZ>);
+
+      tf::StampedTransform transform;
+      try {
+        listener_->lookupTransform (target_frame_,
+                                    cluster_indices->header.frame_id,
+                                    ros::Time(0),
+                                    transform);
+      } catch (tf::TransformException ex){
+        ROS_ERROR("%s",ex.what());
+        return;
+      }
+
+      pcl_ros::transformPointCloud(*filtered_cloud, *transformed_cloud, transform);
+      filtered_cloud->clear();
       jsk_recognition_msgs::BoundingBox box;
       pcl_msgs::PointIndices point_indices_msg;
 
@@ -79,39 +119,40 @@ void callback(const jsk_recognition_msgs::ClusterPointIndices::ConstPtr& cluster
           }
         }
 
-        float max_x = 0.0;
-        float max_y = 0.0;
-        float max_z = 0.0;
-        float min_x = std::pow(2, 24);;
-        float min_y = std::pow(2, 24);;
-        float min_z = std::pow(2, 24);
+        Points max_p(0.0, 0.0, 0.0);
+        Points min_p(std::pow(2, 24), std::pow(2, 24), std::pow(2, 24));
 
         for(int i=0; i < output_indices.at(index).indices.size(); i++){
           point_indices_msg.indices.push_back(output_indices.at(index).indices[i]);
           int indices = output_indices.at(index).indices[i];
-          float x = filtered_cloud->points.at(indices).x;
-          float y = filtered_cloud->points.at(indices).y;
-          float z = filtered_cloud->points.at(indices).z;
-          max_x = x > max_x ? x : max_x;
-          max_y = y > max_y ? y : max_y;
-          max_z = z > max_z ? z : max_z;
-          min_x = x < min_x ? x : min_x;
-          min_y = y < min_y ? y : min_y;
-          min_z = z < min_z ? z : min_z;
+          float x = transformed_cloud->points.at(indices).x;
+          float y = transformed_cloud->points.at(indices).y;
+          float z = transformed_cloud->points.at(indices).z;
+          max_p.x = x > max_p.x ? x : max_p.x;
+          max_p.y = y > max_p.y ? y : max_p.y;
+          max_p.z = z > max_p.z ? z : max_p.z;
+          min_p.x = x < min_p.x ? x : min_p.x;
+          min_p.y = y < min_p.y ? y : min_p.y;
+          min_p.z = z < min_p.z ? z : min_p.z;
         }
-        filtered_cloud->clear();
-        
-        box.dimensions.x = max_x - min_x;
-        box.dimensions.y = max_y - min_y;
-        box.dimensions.z = max_z - min_z;
-        box.pose.position.x = min_x + (max_x - min_x) * 0.5;
-        box.pose.position.y = min_y + (max_y - min_y) * 0.5;
-        box.pose.position.z = min_z + (max_z - min_z) * 0.5;
+
+        Points new_max_p(0, 0, 0);
+        Points new_min_p(std::pow(2, 24), std::pow(2, 24), std::pow(2, 24));
+        std::vector<float> vec = {max_p.x, max_p.y, max_p.z, min_p.x, min_p.y, min_p.z};
+
+        box.dimensions.x = std::fabs(max_p.x - min_p.x);
+        box.dimensions.y = std::fabs(max_p.y - min_p.y);
+        box.dimensions.z = std::fabs(max_p.z - min_p.z);
+        box.pose.position.x = std::min(min_p.x, max_p.x) + box.dimensions.x * 0.5;
+        box.pose.position.y = std::min(min_p.y, max_p.y) + box.dimensions.y * 0.5;
+        box.pose.position.z = std::min(min_p.z, max_p.z) + box.dimensions.z * 0.5;
         box.pose.orientation.x = 1.0;
         box.pose.orientation.y = 0.0;
         box.pose.orientation.z = 0.0;
         box.pose.orientation.w = 0.0;
+        box.label = labels->labels[label_index].id;
         box.header = cluster_indices->header;
+        box.header.frame_id = target_frame_;
       } else {
         box.dimensions.x = 0;
         box.dimensions.y = 0;
@@ -123,6 +164,9 @@ void callback(const jsk_recognition_msgs::ClusterPointIndices::ConstPtr& cluster
         box.pose.orientation.y = 0.0;
         box.pose.orientation.z = 0.0;
         box.pose.orientation.w = 0.0;
+        box.label = labels->labels[label_index].id;
+        box.header = cluster_indices->header;
+        box.header.frame_id = target_frame_;
         output_box_msg.boxes.push_back(box);
 
         ROS_ERROR("output_indices.size() == 0");
@@ -131,9 +175,10 @@ void callback(const jsk_recognition_msgs::ClusterPointIndices::ConstPtr& cluster
 
       point_indices_msg.header = cluster_indices->header;
       output_cluster_indices.cluster_indices.push_back(point_indices_msg);
-
+      label_index++;
     }
     output_box_msg.header = cluster_indices->header;
+    output_box_msg.header.frame_id = target_frame_;
     output_box_pub.publish(output_box_msg);
     output_cluster_indices.header = cluster_indices->header;
     output_cluster_indices_pub.publish(output_cluster_indices);
@@ -142,7 +187,8 @@ void callback(const jsk_recognition_msgs::ClusterPointIndices::ConstPtr& cluster
 
 typedef message_filters::sync_policies::ApproximateTime<
   jsk_recognition_msgs::ClusterPointIndices,
-  sensor_msgs::PointCloud2
+  sensor_msgs::PointCloud2,
+  jsk_recognition_msgs::LabelArray
   > SyncPolicy;
 
 int main(int argc, char** argv)
@@ -150,16 +196,22 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "multi_euclidean_clustering");
 
   ros::NodeHandle nh("~");
+
+  tf::TransformListener lis;
+  listener_ = &lis;
+
   output_cluster_indices_pub = nh.advertise<jsk_recognition_msgs::ClusterPointIndices>("output_indices",1);
   output_box_pub = nh.advertise<jsk_recognition_msgs::BoundingBoxArray>("output_box",1);
 
   message_filters::Subscriber<jsk_recognition_msgs::ClusterPointIndices> sub_cluster_indices;
   message_filters::Subscriber<sensor_msgs::PointCloud2> sub_point_cloud;
+  message_filters::Subscriber<jsk_recognition_msgs::LabelArray> sub_labels;
   sub_cluster_indices.subscribe(nh, "input_cluster_indices", 1);
   sub_point_cloud.subscribe(nh, "input_point_cloud", 1);
+  sub_labels.subscribe(nh, "input_instance_labels", 1);
   boost::shared_ptr<message_filters::Synchronizer<SyncPolicy> >sync_
     = boost::make_shared<message_filters::Synchronizer<SyncPolicy> >(1000);
-  sync_->connectInput(sub_cluster_indices, sub_point_cloud);
+  sync_->connectInput(sub_cluster_indices, sub_point_cloud, sub_labels);
   sync_->registerCallback(callback);
 
   ros::spin();
