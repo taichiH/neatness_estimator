4ba846f70eb4092d8e6cc976797310a6f156da29
taichiH <higashide@jsk.imi.i.u-tokyo.ac.jp>
2019-01-31 18:27:17 +0900

    need debug

--- neatness_estimator/scripts/neatness_estimator2.py
+++ neatness_estimator/scripts/neatness_estimator2.py
@@ -12,6 +12,7 @@ from neatness_estimator_msgs.msg import Neatness
 from geometry_msgs.msg import Pose
 from geometry_msgs.msg import Point
 from distance_clustering import Clustering
+from neatness_estimator_msgs.msg import TargetAndGoal
 
 from visualization_msgs.msg import Marker
 
@@ -31,10 +32,16 @@ class NeatnessEstimator():
         self.marker_pub = rospy.Publisher('~output_marker',
                                           Marker,
                                           queue_size=1)
+        self.target_and_goal_pub = rospy.Publisher('~target_and_goal',
+                                                   TargetAndGoal,
+                                                   queue_size=1)
 
         self.clustering = Clustering()
         self.second_cluster_limit = rospy.get_param('~second_cluster_limit', 0.04)
         self.thresh = rospy.get_param('~thresh', 0.8)
+        self.target_object = 'mixjuice'
+        self.target = None
+        self.goal = None
         self.boxes = []
         self.subscribe()
 
@@ -73,8 +80,9 @@ class NeatnessEstimator():
                 label_buf.append(box.label)
 
         bounding_box_msg = BoundingBoxArray()
+        target_and_goal_msg = TargetAndGoal()
         for label, boxes in zip(labeled_boxes.keys(), labeled_boxes.values()):
-            if self.label_lst[label] != 'mixjuice':
+            if self.label_lst[label] != self.target_object:
                 continue
 
             thresh = self.thresh
@@ -87,39 +95,47 @@ class NeatnessEstimator():
             second_thresh = 0.4
             step = 0.01
 
+            max_distance = 0
             for i, cluster in enumerate(result):                
-                # print('-------------- %s' %(self.label_lst[label]))
+                print('-------------- %s' %(self.label_lst[label]))
                 clustered_boxes = np.array([np.array(boxes[i]) for i in cluster.indices])
                 if len(clustered_boxes) > 1:
-                    second_result = self.second_clustering(clustered_boxes, second_thresh, step)
+                    second_result, distance = self.second_clustering(clustered_boxes, second_thresh, step)
                     if len(second_result) > 1:
                         transformed_pos = []
                         for devided_cluster in second_result:
                             pos = []
+                            faile_transform = False
                             for j in devided_cluster.indices:
                                 name = 'item_' + str(j)
                                 (trans, rot) = self.transform(clustered_boxes[j], name, instance_msg.header)
+                                if len(trans) == 0 and len(rot) == 0:
+                                    rospy.logwarn('listen transform failed')
+                                    faile_transform = True
+
                                 pos.append(np.array(trans))
                             transformed_pos.append(pos)
-                        (target, goal) = self.get_target_and_goal(second_result, transformed_pos)
-                        if target.sum() == 0 and goal.sum() == 0:
+
+                            if faile_transform:
+                                continue
+
+                        target, goal = self.get_target_and_goal(second_result, transformed_pos)
+
+                        if target.sum() == 0 or \
+                           goal.sum() == 0 or \
+                           True in np.isnan(target) or \
+                           True in np.isnan(goal):
                             rospy.logwarn('failed get target and goal')
+                            continue
 
-                        self.broadcaster.sendTransform((target[0],
-                                                        target[1],
-                                                        target[2]),
-                                                       (1, 0, 0, 0),
-                                                       rospy.Time.now(),
-                                                       "target",
-                                                       "base_link")
-                        self.broadcaster.sendTransform((goal[0],
-                                                        goal[1],
-                                                        goal[2]),
-                                                       (1, 0, 0, 0),
-                                                       rospy.Time.now(),
-                                                       "goal",
-                                                       "base_link")
+                        if distance > max_distance:
+                            print(distance)
+                            print(target,goal)
+                            target_and_goal_msg.target.point = Point(target[0], target[1], target[2])
+                            target_and_goal_msg.goal.point = Point(goal[0], goal[1], goal[2])
+                            target_and_goal_msg.target.header = target_and_goal_msg.goal.header = instance_msg.header
 
+                        
                 max_candidates = [boxes[i][0] + (boxes[i][1] * 0.5) for i in cluster.indices]
                 min_candidates = [boxes[i][0] - (boxes[i][1] * 0.5) for i in cluster.indices]
                 candidates = np.array(max_candidates + min_candidates)
@@ -137,8 +153,34 @@ class NeatnessEstimator():
                 tmp_box.label = label
                 bounding_box_msg.boxes.append(tmp_box)
 
+
+        # print('broadcast target and goal')
+        # print(target_and_goal_msg.target.point.x,
+        #       target_and_goal_msg.target.point.y,
+        #       target_and_goal_msg.target.point.z)
+        # print(target_and_goal_msg.goal.point.x,
+        #       target_and_goal_msg.goal.point.y,
+        #       target_and_goal_msg.goal.point.z)
+
+        self.broadcaster.sendTransform((target_and_goal_msg.target.point.x,
+                                        target_and_goal_msg.target.point.y,
+                                        target_and_goal_msg.target.point.z),
+                                       (1, 0, 0, 0),
+                                       rospy.Time.now(),
+                                       "target",
+                                       "base_link")
+        self.broadcaster.sendTransform((target_and_goal_msg.goal.point.x,
+                                        target_and_goal_msg.goal.point.y,
+                                        target_and_goal_msg.goal.point.z),
+                                       (1, 0, 0, 0),
+                                       rospy.Time.now(),
+                                       "goal",
+                                       "base_link")
+
         bounding_box_msg.header = instance_msg.header
         self.box_pub.publish(bounding_box_msg)
+        target_and_goal_msg.target.header.frame_id = target_and_goal_msg.goal.header.frame_id = "base_link"
+        self.target_and_goal_pub.publish(target_and_goal_msg)
 
 
     def transform(self, box, name, header):
@@ -149,23 +191,26 @@ class NeatnessEstimator():
                                        rospy.Time.now(),
                                        name,
                                        header.frame_id)
-        self.listener.waitForTransform("base_link",
-                                       name,
-                                       rospy.Time(0),
-                                       rospy.Duration(5.0))
-        (trans, rot) = self.listener.lookupTransform("base_link",
-                                                     name,
-                                                     rospy.Time(0))
-        return (trans, rot)
+        try:
+            self.listener.waitForTransform("base_link",
+                                           name,
+                                           rospy.Time(0),
+                                           rospy.Duration(5.0))
+            (trans, rot) = self.listener.lookupTransform("base_link",
+                                                         name,
+                                                         rospy.Time(0))
+            return (trans, rot)
+        except:            
+            return ([], [])
 
     def second_clustering(self, clustered_boxes, second_thresh, step):
         second_result = self.clustering.clustering_wrapper(clustered_boxes, second_thresh)
         if second_thresh < self.second_cluster_limit:
-            return second_result
+            return second_result, second_thresh
 
         # when cluster is devided into two
         if len(second_result) > 1:
-            return second_result
+            return second_result, second_thresh
 
         clustered_boxes = np.array([np.array(clustered_boxes[i]) for i in second_result[0].indices])
         return self.second_clustering(clustered_boxes, second_thresh - step, step)
@@ -181,6 +226,10 @@ class NeatnessEstimator():
         if len(second_result[small].indices) > 1:
             rospy.logwarn('there are element in the min cluster more than 1')
             return (np.zeros((3)), np.zeros((3)))
+        elif len(second_result[small].indices) == 0:
+            rospy.logwarn('there is no element in small cluster')
+            return (np.zeros((3)), np.zeros((3)))
+
         pick_target = transformed_pos[small][0]
 
         # sort large cluster elements by x axis on base_link coords
