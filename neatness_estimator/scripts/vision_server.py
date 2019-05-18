e22a413095f7296f0102e228ed94ca3d475229ef
taichiH <higashide@jsk.imi.i.u-tokyo.ac.jp>
2019-05-19 05:09:41 +0900

    revise vision_server

--- neatness_estimator/scripts/vision_server.py
+++ neatness_estimator/scripts/vision_server.py
@@ -21,175 +21,75 @@ class NeatnessEstimatorVisionServer():
 
         rospy.Subscriber("~input_instance_boxes",
                          BoundingBoxArray,
-                         self.mask_rcnn_callback)
+                         self.instance_box_callback)
         rospy.Subscriber("~input_cluster_boxes",
                          BoundingBoxArray,
-                         self.slide_adjust_callback)
+                         self.cluster_box_callback)
         rospy.Service('/display_task_vision_server',
                       VisionServer,
                       self.vision_server)
 
-    def mask_rcnn_callback(self, msg):
+    def instance_box_callback(self, msg):
         self.boxes = msg
 
-    def slide_adjust_callback(self, msg):
+    def cluster_box_callback(self, msg):
         self.labeled_boxes = msg
 
-    def vision_server(self, req):
-        if req.task == 'get_obj_pos':
-            rospy.loginfo(req.task)
-            distance = 100
-            nearest_box = BoundingBox()
-            for index, box in enumerate(self.boxes.boxes):
-                if self.label_lst[box.label] == req.label:
-                    ref_point = np.array([box.pose.position.x + (box.dimensions.x * 0.5),
-                                          box.pose.position.y + (box.dimensions.y * 0.5),
-                                          box.pose.position.z + (box.dimensions.z * 0.5)])
-                    target_point = np.array([req.target.x,
-                                             req.target.y,
-                                             req.target.z])
-                    if np.linalg.norm(ref_point - target_point) < distance:
-                        nearest_box.pose = box.pose
-                        nearest_box.dimensions = box.dimensions
-                        distance = np.linalg.norm(ref_point - target_point)
+    def get_obj_pos(self, req):
+        rospy.loginfo(req.task)
+        distance = 100
+        nearest_box = BoundingBox()
+        for index, box in enumerate(self.boxes.boxes):
+            if self.label_lst[box.label] == req.label:
+                ref_point = np.array([box.pose.position.x + (box.dimensions.x * 0.5),
+                                      box.pose.position.y + (box.dimensions.y * 0.5),
+                                      box.pose.position.z + (box.dimensions.z * 0.5)])
+                target_point = np.array([req.target.x,
+                                         req.target.y,
+                                         req.target.z])
+                if np.linalg.norm(ref_point - target_point) < distance:
+                    nearest_box.pose = box.pose
+                    nearest_box.dimensions = box.dimensions
+                    distance = np.linalg.norm(ref_point - target_point)
 
-            res = VisionServerResponse()
-            transformed_box = self.transform_poses(nearest_box.pose,
-                                                   req.label,
-                                                   self.boxes.header.frame_id,
-                                                   req.parent_frame)
-            transformed_box.header = self.boxes.header
-            transformed_box.header.frame_id = req.parent_frame
-            transformed_box.dimensions = nearest_box.dimensions
-            res.boxes = transformed_box
+        res = VisionServerResponse()
+        transformed_box = self.transform_poses(nearest_box.pose,
+                                               req.label,
+                                               self.boxes.header.frame_id,
+                                               req.parent_frame)
+        transformed_box.header = self.boxes.header
+        transformed_box.header.frame_id = req.parent_frame
+        transformed_box.dimensions = nearest_box.dimensions
+        res.boxes = transformed_box
+        res.status = True
+        return res
+
+    def get_distance_from_shelf_front(self, req):
+        shelf_front = None
+        target = None
+        for box in self.boxes.boxes:
+            if box.label == int(self.label_lst.index('shelf_flont')):
+                shelf_front = np.array([box.pose.position.x,
+                                        box.pose.position.y,
+                                        box.pose.position.z])
+            if box.label == int(self.label_lst.index(req.label)):
+                target = np.array([box.pose.position.x,
+                                   box.pose.position.y,
+                                   box.pose.position.z])
+
+        res = VisionServerResponse()
+        if shelf_front is not None and target is not None:
+            res.pulling_dist = np.linalg.norm(shelf_front - target)
             res.status = True
-            return res
-
-        elif req.task == 'get_dist':
-            rospy.loginfo(req.task)
-
-            target_left = 0
-            target_bottom = 0
-            ref_right = 0
-            ref_bottom = 0
-            for box in self.labeled_boxes.boxes:
-                if box.label == int(self.label_lst.index(req.label)):
-                    target_left = box.pose.position.y + box.dimensions.y * 0.5
-                    target_bottom = box.pose.position.x - box.dimensions.x * 0.5
-                if box.label == int(self.label_lst.index(req.ref_label)):
-                    ref_right = box.pose.position.y - box.dimensions.y * 0.5
-                    ref_bottom = box.pose.position.x - box.dimensions.x * 0.5
-
-            res = VisionServerResponse()
-            res.filling_dist = abs(target_left - ref_right)
-            res.pulling_dist = abs(target_bottom - ref_bottom)
-            res.status = True
-            return res
-
-        elif req.task == 'get_cluster_box':
-            rospy.loginfo(req.task)
-            distance = 100
-            nearest_box = BoundingBox()
-            for index, box in enumerate(self.labeled_boxes.boxes):
-                if self.label_lst[box.label] == req.label:
-                    ref_point = np.array([box.pose.position.x + (box.dimensions.x * 0.5),
-                                          box.pose.position.y + (box.dimensions.y * 0.5),
-                                          box.pose.position.z + (box.dimensions.z * 0.5)])
-                    target_point = np.array([req.target.x,
-                                             req.target.y,
-                                             req.target.z])
-                    if np.linalg.norm(ref_point - target_point) < distance:
-                        nearest_box.pose = box.pose
-                        nearest_box.dimensions = box.dimensions
-                        distance = np.linalg.norm(ref_point - target_point)
-
-            res = VisionServerResponse()
-            transformed_box = self.transform_poses(nearest_box.pose,
-                                                   req.label,
-                                                   self.boxes.header.frame_id,
-                                                   req.parent_frame)
-            transformed_box.header = self.boxes.header
-            transformed_box.header.frame_id = req.parent_frame
-            transformed_box.dimensions = nearest_box.dimensions
-            res.boxes = transformed_box
-            res.status = True
-            return res
-
-
-        elif req.task == 'get_empty_space':
-            rospy.loginfo(req.task)
-            print(len(self.labeled_boxes.boxes))
-            for box in self.labeled_boxes.boxes:
-                if box.label == int(self.label_lst.index(req.label)):
-                    shelf_lt = np.array([box.pose.position.x + box.dimensions.x * 0.5,
-                                         box.pose.position.y + box.dimensions.y * 0.5,
-                                         box.pose.position.z + box.dimensions.z * 0.5])
-                    shelf_rb = np.array([box.pose.position.x - box.dimensions.x * 0.5,
-                                         box.pose.position.y - box.dimensions.y * 0.5,
-                                         box.pose.position.z - box.dimensions.z * 0.5])
-                    break
-
-            cliped_bounding_box = BoundingBoxArray()
-            offset_y = 0.5
-            offset_z = 0.15
-
-            for box in self.labeled_boxes.boxes:
-                if box.label == int(self.label_lst.index(req.label)):
-                    continue
-
-                if box.pose.position.y < (shelf_lt[1] + offset_y) and \
-                   box.pose.position.y > (shelf_rb[1] - offset_y) and \
-                   box.pose.position.z > (shelf_lt[2] - offset_z):
-                    cliped_bounding_box.boxes.append(box)
-
-            sorted_bounding_box = BoundingBoxArray()
-            sorted_bounding_box.boxes = sorted(cliped_bounding_box.boxes,
-                                               key=lambda box : box.pose.position.y)
-
-            max_dist = 0
-            index = 0
-            for i in range(1, len(sorted_bounding_box.boxes)):
-                dist = (sorted_bounding_box.boxes[i].pose.position.y - \
-                        sorted_bounding_box.boxes[i].dimensions.y * 0.5) - \
-                        (sorted_bounding_box.boxes[i-1].pose.position.y + \
-                         sorted_bounding_box.boxes[i-1].dimensions.y * 0.5)
-                if dist > max_dist:
-                    index = i
-                    max_dist = dist
-
-            print('index: ', index)
-            res = VisionServerResponse()
-            empty_space_box = BoundingBox()
-            empty_space_box.header = self.labeled_boxes.header
-
-            empty_space_box.pose.position.x = (sorted_bounding_box.boxes[index].pose.position.x + \
-                                               sorted_bounding_box.boxes[index-1].pose.position.x) * 0.5
-            empty_space_box.pose.position.y = (sorted_bounding_box.boxes[index].pose.position.y + \
-                                               sorted_bounding_box.boxes[index-1].pose.position.y) * 0.5
-            empty_space_box.pose.position.z = (sorted_bounding_box.boxes[index].pose.position.z + \
-                                               sorted_bounding_box.boxes[index-1].pose.position.z) * 0.5
-            empty_space_box.pose.orientation = sorted_bounding_box.boxes[index].pose.orientation
-            empty_space_box.dimensions.x = (sorted_bounding_box.boxes[index].dimensions.x + \
-                                            sorted_bounding_box.boxes[index-1].dimensions.x) * 0.5
-            empty_space_box.dimensions.y = max_dist
-            empty_space_box.dimensions.z = (sorted_bounding_box.boxes[index].dimensions.z + \
-                                            sorted_bounding_box.boxes[index-1].dimensions.z) * 0.5
-
-            # transform map -> base_link
-            transformed_box = self.transform_poses(empty_space_box.pose,
-                                                   "empty_space",
-                                                   empty_space_box.header.frame_id,
-                                                   req.parent_frame)
-            empty_space_box.pose = transformed_box.pose
-            res.boxes = empty_space_box
-            res.status = True
-            return res
-
         else:
-            rospy.loginfo('not much task')
-            res = VisionServerResponse()
             res.status = False
-            return res
+        return res
+
+    def get_empty_space(self, req):
+        rospy.loginfo(req.task)
+        rospy.logerr('get_empty_space is not used')
+        res.status = False
+        return res
 
     def listen_transform(self, parent_frame, child_frame):
         box = BoundingBox()
@@ -227,6 +127,18 @@ class NeatnessEstimatorVisionServer():
 
         box =  self.listen_transform(parent, label)
         return box
+
+    def vision_server(self, req):
+        if req.task == 'get_obj_pos':
+            return self.get_obj_pos(req)
+
+        elif req.task == 'get_distance':
+            rospy.loginfo(req.task)
+            return self.get_distance_from_shelf_front(req)
+
+        elif req.task == 'get_empty_space':
+            rospy.loginfo(req.task)
+            return self.get_empty_space(req)
 
 if __name__ == "__main__":
     rospy.init_node("display_task_vision_server")
