f67ce10d469f6b788726d95a503ea326b94c36af
taichiH <higashide@jsk.imi.i.u-tokyo.ac.jp>
2019-05-20 01:41:27 +0900

    revise vision_server.py

--- neatness_estimator/scripts/vision_server.py
+++ neatness_estimator/scripts/vision_server.py
@@ -7,6 +7,7 @@ import rospy
 import tf
 
 from jsk_recognition_msgs.msg import BoundingBox, BoundingBoxArray
+from geometry_msgs.msg import Pose, Point, Quaternion
 from neatness_estimator_msgs.srv import VisionServer, VisionServerResponse
 
 class NeatnessEstimatorVisionServer():
@@ -15,19 +16,15 @@ class NeatnessEstimatorVisionServer():
         self.label_lst = rospy.get_param('~fg_class_names')
         self.boxes = BoundingBoxArray()
         self.labeled_boxes = BoundingBoxArray()
-
         self.listener = tf.TransformListener()
         self.broadcaster = tf.TransformBroadcaster()
 
-        rospy.Subscriber("~input_instance_boxes",
-                         BoundingBoxArray,
-                         self.instance_box_callback)
-        rospy.Subscriber("~input_cluster_boxes",
-                         BoundingBoxArray,
-                         self.cluster_box_callback)
-        rospy.Service('/display_task_vision_server',
-                      VisionServer,
-                      self.vision_server)
+        rospy.Subscriber(
+            "~input_instance_boxes", BoundingBoxArray, self.instance_box_callback)
+        rospy.Subscriber(
+            "~input_cluster_boxes", BoundingBoxArray, self.cluster_box_callback)
+        rospy.Service(
+            '/display_task_vision_server', VisionServer, self.vision_server)
 
     def instance_box_callback(self, msg):
         self.boxes = msg
@@ -35,8 +32,87 @@ class NeatnessEstimatorVisionServer():
     def cluster_box_callback(self, msg):
         self.labeled_boxes = msg
 
+
+    ''' task get_obj_pos '''
     def get_obj_pos(self, req):
         rospy.loginfo(req.task)
+        res = VisionServerResponse()
+
+        try:
+            nearest_box = self.get_nearest_box(req)
+
+            if req.parent_frame == '':
+                transformed_box = nearest_box
+            else:
+                transformed_box = self.transform_poses(
+                    nearest_box.pose, req.label, self.boxes.header.frame_id, req.parent_frame)
+
+            # faile lookup transform
+            if transformed_box == BoundingBox():
+                transformed_box = nearest_box
+                transformed_box.header = self.boxes.header
+            else:
+                transformed_box.header = self.boxes.header
+                transformed_box.header.frame_id = req.parent_frame
+                transformed_box.dimensions = nearest_box.dimensions
+
+            res.boxes = transformed_box
+            res.status = True
+        except:
+            res.status = False
+            import traceback
+            traceback.print_exc()
+
+        return res
+
+    ''' task get_distance_from_shelf_front '''
+    def get_distance_from_shelf_front(self, req):
+        rospy.loginfo(req.task)
+        res = VisionServerResponse()
+
+        try:
+            has_shelf = False
+            for box in self.boxes.boxes:
+                if box.label == int(self.label_lst.index('shelf_flont')):
+                    has_shelf = True
+                    shelf_front = np.array([box.pose.position.x, box.pose.position.y])
+                    shelf_edge = np.array([box.pose.position.x, box.pose.position.y]) +\
+                                 np.array([box.dimensions.x, box.dimensions.y])
+
+            if not has_shelf:
+                rospy.logwarn("cannot find shelf front")
+                res.status = False
+                return res
+
+            nearest_box = self.get_nearest_box(req)
+            target_vec = np.array([nearest_box.pose.position.x, nearest_box.pose.position.y])
+            target_vec = target_vec - shelf_front
+            shelf_vec = shelf_edge - shelf_front
+            theta = np.arccos(
+                np.dot(target_vec, shelf_vec) / np.linalg.norm(target_vec) * np.linalg.norm(shelf_vec))
+            theta = np.deg2rad(180) - theta if theta > np.deg2rad(90) else theta
+            distance  = np.linalg.norm(target_vec) * np.sin(theta)
+
+            if shelf_front is not None and target_vec is not None:
+                res.pulling_dist = distance
+                res.status = True
+            else:
+                res.status = False
+        except:
+            res.status = False
+            import traceback
+            traceback.print_exc()
+
+        return res
+
+    ''' task get_empty_space '''
+    def get_empty_space(self, req):
+        rospy.loginfo(req.task)
+        rospy.logerr('get_empty_space is not used')
+        res.status = False
+        return res
+
+    def get_nearest_box(self, req):
         distance = 100
         nearest_box = BoundingBox()
         for index, box in enumerate(self.boxes.boxes):
@@ -51,80 +127,27 @@ class NeatnessEstimatorVisionServer():
                     nearest_box.pose = box.pose
                     nearest_box.dimensions = box.dimensions
                     distance = np.linalg.norm(ref_point - target_point)
-
-        res = VisionServerResponse()
-        transformed_box = self.transform_poses(nearest_box.pose,
-                                               req.label,
-                                               self.boxes.header.frame_id,
-                                               req.parent_frame)
-        transformed_box.header = self.boxes.header
-        transformed_box.header.frame_id = req.parent_frame
-        transformed_box.dimensions = nearest_box.dimensions
-        res.boxes = transformed_box
-        res.status = True
-        return res
-
-    def get_distance_from_shelf_front(self, req):
-        shelf_front = None
-        target = None
-        for box in self.boxes.boxes:
-            if box.label == int(self.label_lst.index('shelf_flont')):
-                shelf_front = np.array([box.pose.position.x,
-                                        box.pose.position.y,
-                                        box.pose.position.z])
-            if box.label == int(self.label_lst.index(req.label)):
-                target = np.array([box.pose.position.x,
-                                   box.pose.position.y,
-                                   box.pose.position.z])
-
-        res = VisionServerResponse()
-        if shelf_front is not None and target is not None:
-            res.pulling_dist = np.linalg.norm(shelf_front - target)
-            res.status = True
-        else:
-            res.status = False
-        return res
-
-    def get_empty_space(self, req):
-        rospy.loginfo(req.task)
-        rospy.logerr('get_empty_space is not used')
-        res.status = False
-        return res
+        return nearest_box
 
     def listen_transform(self, parent_frame, child_frame):
         box = BoundingBox()
         try:
-            self.listener.waitForTransform(parent_frame,
-                                           child_frame,
-                                           rospy.Time(0),
-                                           rospy.Duration(3.0))
-            (trans, rot) = self.listener.lookupTransform(parent_frame,
-                                                         child_frame,
-                                                         rospy.Time(0))
-            box.pose.position.x = trans[0]
-            box.pose.position.y = trans[1]
-            box.pose.position.z = trans[2]
-            box.pose.orientation.x = rot[0]
-            box.pose.orientation.y = rot[1]
-            box.pose.orientation.z = rot[2]
-            box.pose.orientation.w = rot[3]
+            self.listener.waitForTransform(
+                parent_frame, child_frame, rospy.Time(0), rospy.Duration(3.0))
+            (trans, rot) = self.listener.lookupTransform(
+                parent_frame, child_frame, rospy.Time(0))
+            box.pose.position = Point(trans[0], trans[1], trans[2])
+            box.pose.orientation = Quaternion(rot[0], rot[1], rot[2], rot[3])
             return box
         except:
             rospy.logwarn('cannot lookup transform')
             return box
 
     def transform_poses(self, pose, label, frame_id, parent):
-        self.broadcaster.sendTransform((pose.position.x,
-                                        pose.position.y,
-                                        pose.position.z),
-                                       (pose.orientation.x,
-                                        pose.orientation.y,
-                                        pose.orientation.z,
-                                        pose.orientation.w),
-                                       rospy.Time.now(),
-                                       label,
-                                       frame_id)
-
+        self.broadcaster.sendTransform(
+            (pose.position.x, pose.position.y, pose.position.z),
+            (pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w),
+            rospy.Time.now(), label, frame_id)
         box =  self.listen_transform(parent, label)
         return box
 
