2c0631badf0213d34e03cee259bc655ccbde77ab
taichiH <higashide@jsk.imi.i.u-tokyo.ac.jp>
2019-10-11 00:53:40 +0900

    [data_saver] change save format from original to rosbag

--- neatness_estimator/include/neatness_estimator/data_saver.h
+++ neatness_estimator/include/neatness_estimator/data_saver.h
@@ -3,33 +3,21 @@
 
 #include <ros/ros.h>
 #include <nodelet/nodelet.h>
-#include <cv_bridge/cv_bridge.h>
-#include <sensor_msgs/image_encodings.h>
+#include <rosbag/bag.h>
 
 #include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
 #include <message_filters/synchronizer.h>
 #include <message_filters/sync_policies/approximate_time.h>
 
-#include <jsk_recognition_msgs/BoundingBoxArray.h>
+#include <jsk_recognition_msgs/ClusterPointIndices.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/Image.h>
-#include <std_msgs/Header.h>
 #include <std_srvs/SetBool.h>
-
-#include <pcl_conversions/pcl_conversions.h>
-#include <pcl_msgs/PointIndices.h>
-#include <pcl/io/pcd_io.h>
-#include <pcl/point_types.h>
-#include <pcl/io/io.h>
-
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/core/core.hpp>
-#include <opencv/cv.hpp>
 
 #include <mutex>
 #include <boost/filesystem.hpp>
-#include <yaml-cpp/yaml.h>
+
 
 namespace neatness_estimator
 {
@@ -38,16 +26,22 @@ namespace neatness_estimator
   public:
 
     typedef message_filters::sync_policies::ExactTime<
-      jsk_recognition_msgs::BoundingBoxArray,
       sensor_msgs::PointCloud2,
-      sensor_msgs::Image
+      sensor_msgs::Image,
+      jsk_recognition_msgs::ClusterPointIndices
       > SyncPolicy;
 
     typedef message_filters::sync_policies::ApproximateTime<
-      jsk_recognition_msgs::BoundingBoxArray,
       sensor_msgs::PointCloud2,
-      sensor_msgs::Image
+      sensor_msgs::Image,
+      jsk_recognition_msgs::ClusterPointIndices
       > ApproximateSyncPolicy;
+
+    enum Topics {
+      CLOUD,
+      IMAGE,
+      CLUSTER
+    };
 
   protected:
 
@@ -55,22 +49,15 @@ namespace neatness_estimator
 
     virtual void onInit();
 
-    virtual void callback(const jsk_recognition_msgs::BoundingBoxArray::ConstPtr& boxes_msg,
-                          const sensor_msgs::PointCloud2::ConstPtr& cloud_msg,
-                          const sensor_msgs::Image::ConstPtr& image_msg);
+    virtual void callback(const sensor_msgs::PointCloud2::ConstPtr& cloud_msg,
+                          const sensor_msgs::Image::ConstPtr& image_msg,
+                          const jsk_recognition_msgs::ClusterPointIndices::ConstPtr& cluster_msg);
 
     virtual bool service_callback(std_srvs::SetBool::Request& req,
                                   std_srvs::SetBool::Response& res);
 
     virtual bool create_save_dir(std::stringstream& ss,
                                  std::string dir_name);
-
-    virtual bool save_pcd(std::string save_path);
-
-    virtual bool save_image(std::string save_path);
-
-    virtual bool save_boxes(std::string save_path);
-
 
 
     // variables
@@ -85,29 +72,23 @@ namespace neatness_estimator
 
     boost::shared_ptr<message_filters::Synchronizer<ApproximateSyncPolicy> > async_;
 
-    message_filters::Subscriber<jsk_recognition_msgs::BoundingBoxArray> sub_boxes_;
-
     message_filters::Subscriber<sensor_msgs::PointCloud2> sub_point_cloud_;
 
     message_filters::Subscriber<sensor_msgs::Image> sub_image_;
 
+    message_filters::Subscriber<jsk_recognition_msgs::ClusterPointIndices> sub_cluster_;
 
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_;
+    sensor_msgs::PointCloud2::ConstPtr cloud_msg_;
 
-    cv::Mat image_;
+    sensor_msgs::Image::ConstPtr image_msg_;
 
-    jsk_recognition_msgs::BoundingBoxArray::ConstPtr boxes_;
-
+    jsk_recognition_msgs::ClusterPointIndices::ConstPtr cluster_msg_;
 
     boost::mutex mutex_;
 
-    bool binary_ = false;
-
-    bool compressed_ = false;
-
     std::string prefix_ = "./";
 
-    std_msgs::Header header_;
+    std::vector<std::string> topics_;
 
   private:
 
--- neatness_estimator/src/data_saver.cpp
+++ neatness_estimator/src/data_saver.cpp
@@ -7,26 +7,32 @@ namespace neatness_estimator
   {
     nh_ = getNodeHandle();
     pnh_ = getPrivateNodeHandle();
-
-    pnh_.getParam("binary", binary_);
     pnh_.getParam("prefix", prefix_);
-    pnh_.getParam("compressed", compressed_);
 
     server_ = pnh_.advertiseService("save", &DataSaver::service_callback, this);
 
     sub_point_cloud_.subscribe(pnh_, "input_cloud", 1);
     sub_image_.subscribe(pnh_, "input_image", 1);
-    sub_boxes_.subscribe(pnh_, "input_boxes", 1);
+    sub_cluster_.subscribe(pnh_, "input_cluster", 1);
+
+    // topic name added to topic_manager when subscribe function is called
+    ros::this_node::getSubscribedTopics(topics_);
+    for (size_t i=0; i<topics_.size(); ++i) {
+      std::cerr << i << ", " << topics_.at(i) << std::endl;
+    }
 
     bool approximate_sync;
     pnh_.getParam("approximate_sync", approximate_sync);
+    std::cerr << "approximate_sync: " << approximate_sync << std::endl;
+
     if (approximate_sync) {
       async_ = boost::make_shared<message_filters::Synchronizer<ApproximateSyncPolicy> >(1000);
-      async_->connectInput(sub_boxes_, sub_point_cloud_, sub_image_);
+      async_->connectInput(sub_point_cloud_, sub_image_, sub_cluster_);
       async_->registerCallback(boost::bind(&DataSaver::callback, this, _1, _2, _3));
     } else {
+
       sync_  = boost::make_shared<message_filters::Synchronizer<SyncPolicy> >(1000);
-      sync_->connectInput(sub_boxes_, sub_point_cloud_, sub_image_);
+      sync_->connectInput(sub_point_cloud_, sub_image_, sub_cluster_);
       sync_->registerCallback(boost::bind(&DataSaver::callback, this, _1, _2, _3));
     }
 
@@ -60,119 +66,18 @@ namespace neatness_estimator
   }
 
 
-  bool DataSaver::save_pcd(std::string save_path)
-  {
-    ROS_INFO("save pcd path: \n%s", save_path.c_str());
-    pcl::PCDWriter writer;
-    try {
-      if(binary_) {
-        if(compressed_) {
-          writer.writeBinaryCompressed(save_path, *cloud_);
-        } else {
-          writer.writeBinary(save_path, *cloud_);
-        }
-      } else {
-        writer.writeASCII(save_path, *cloud_);
-      }
-    } catch (...) {
-      return false;
-    }
-    return true;
-  }
 
-  bool DataSaver::save_image(std::string save_path)
-  {
-    ROS_INFO("save image path: \n%s", save_path.c_str());
-    cv::imwrite(save_path, image_);
-    return true;
-  }
-
-
-  bool DataSaver::save_boxes(std::string save_path)
-  {
-    ROS_INFO("save yaml path: \n%s", save_path.c_str());
-
-    std::map <std::string, std::string> header_map;
-    header_map["frame_id"] = boxes_->header.frame_id;
-    header_map["stamp_sec"] = std::to_string(boxes_->header.stamp.sec);
-    header_map["stamp_nsec"] = std::to_string(boxes_->header.stamp.nsec);
-
-
-    YAML::Emitter emitter;
-    emitter.SetOutputCharset(YAML::EscapeNonAscii);
-    emitter.SetIndent(2);
-
-    // start all map
-    emitter << YAML::BeginMap; // all
-
-    // start header map
-    emitter << YAML::Key << "header";
-    emitter << YAML::Value << header_map;
-
-    // start boxes map
-    emitter << YAML::Key << "boxes";
-    emitter << YAML::Value;
-
-    emitter << YAML::Flow; // list
-    emitter << YAML::BeginSeq; // list begin
-    for (auto box : boxes_->boxes){
-      std::map<std::string, std::string> box_map;
-      box_map["x"] = std::to_string(box.pose.position.x);
-      box_map["y"] = std::to_string(box.pose.position.y);
-      box_map["z"] = std::to_string(box.pose.position.z);
-      box_map["qx"] = std::to_string(box.pose.orientation.x);
-      box_map["qy"] = std::to_string(box.pose.orientation.y);
-      box_map["qz"] = std::to_string(box.pose.orientation.z);
-      box_map["qw"] = std::to_string(box.pose.orientation.w);
-      box_map["dimx"] = std::to_string(box.dimensions.x);
-      box_map["dimy"] = std::to_string(box.dimensions.y);
-      box_map["dimz"] = std::to_string(box.dimensions.z);
-
-      // start box map
-      emitter << YAML::BeginMap;
-      emitter << YAML::Key << "box";
-      emitter << YAML::Value << box_map;
-      emitter << YAML::EndMap;
-      // end box map
-    }
-    emitter << YAML::EndSeq; // list end
-    // end boxes map
-
-    emitter << YAML::EndMap; // all
-    // end all map
-
-    std::ofstream fo(save_path);
-    fo << emitter.c_str();
-
-    return true;
-  }
-
-
-
-  void DataSaver::callback(const jsk_recognition_msgs::BoundingBoxArray::ConstPtr& boxes_msg,
-                           const sensor_msgs::PointCloud2::ConstPtr& cloud_msg,
-                           const sensor_msgs::Image::ConstPtr& image_msg)
+  void DataSaver::callback(const sensor_msgs::PointCloud2::ConstPtr& cloud_msg,
+                           const sensor_msgs::Image::ConstPtr& image_msg,
+                           const jsk_recognition_msgs::ClusterPointIndices::ConstPtr& cluster_msg)
   {
     boost::mutex::scoped_lock lock(mutex_);
 
-    header_ = cloud_msg->header;
+    std::cerr << "callback!!! " << std::endl;
 
-    std::cerr << "callback!!!" << std::endl;
-    cloud_.reset(new pcl::PointCloud<pcl::PointXYZRGB>);
-    pcl::fromROSMsg(*cloud_msg, *cloud_);
-
-
-    try {
-      cv_bridge::CvImagePtr cv_image = cv_bridge::toCvCopy(image_msg, "bgr8");
-      image_ = cv_image->image;
-    } catch (cv_bridge::Exception& e) {
-      ROS_ERROR("Failed to convert sensor_msgs::Image to cv::Mat \n%s", e.what());
-      cloud_->points.clear();
-      return;
-    }
-
-    boxes_ = boxes_msg;
-
+    cloud_msg_ = cloud_msg;
+    image_msg_ = image_msg;
+    cluster_msg_ = cluster_msg;
   }
 
 
@@ -183,38 +88,27 @@ namespace neatness_estimator
 
     std::cerr << "service_callback!!!" << std::endl;
 
-    if ( cloud_->points.empty() ) {
+    if ( cloud_msg_->height * cloud_msg_->width == 0 ) {
       res.success = false;
       return false;
     }
 
     std::stringstream ss;
-    std::string dir_name = std::to_string(header_.stamp.sec);
+    std::string dir_name = std::to_string(cloud_msg_->header.stamp.sec);
     if ( !create_save_dir(ss, dir_name) ) {
       res.success = false;
     }
 
-    std::stringstream pcd_save_path;
-    pcd_save_path << ss.str() << "/" << header_.stamp.sec << ".pcd";
-    if ( !save_pcd(pcd_save_path.str()) ) {
-      res.success = false;
-      return false;
-    }
+    std::stringstream bag_save_path;
+    bag_save_path << ss.str() << "/" << cloud_msg_->header.stamp.sec << ".bag";
+    rosbag::Bag bag;
+    bag.open(bag_save_path.str(), rosbag::bagmode::Write);
+    // topics : [clock, cloud, rgb, cluster]
+    bag.write(topics_.at(1), cloud_msg_->header.stamp, *cloud_msg_);
+    bag.write(topics_.at(2), image_msg_->header.stamp, *image_msg_);
+    bag.write(topics_.at(3), cluster_msg_->header.stamp, *cluster_msg_);
+    bag.close();
 
-
-    std::stringstream image_save_path;
-    image_save_path << ss.str() << "/" << header_.stamp.sec << ".jpg";
-    if ( !save_image(image_save_path.str()) ) {
-      res.success = false;
-      return false;
-    }
-
-    std::stringstream boxes_save_path;
-    boxes_save_path << ss.str() << "/" << header_.stamp.sec << ".yaml";
-    if ( !save_boxes(boxes_save_path.str()) ) {
-      res.success = false;
-      return false;
-    }
 
     res.success = true;
     return true;
