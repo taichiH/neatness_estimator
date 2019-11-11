60477ab8ad1e8d114c57267c500391f99d57fe89
taichiH <higashide@jsk.imi.i.u-tokyo.ac.jp>
2019-11-11 23:47:01 +0900

    add random forest motion classifier

--- neatness_estimator/scripts/get_motion_primitive.py
+++ neatness_estimator/scripts/get_motion_primitive.py
@@ -1,28 +1,67 @@
 #!/usr/bin/env python
 
+import os
+import csv
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
+
 import rospy
+import rospkg
 from neatness_estimator_msgs.srv import GetMotionPrimitive, GetMotionPrimitiveResponse
 
 class GetMotionPrimitiveServer():
 
     def __init__(self):
-        rospy.Service('~', GetMotionPrimitive, self.service_callback)
+        self.motion_lst = ['rot', 'trans', 'nothing']
+        self.model_path = rospy.get_param(
+            '~model_path',
+            os.path.join(
+                rospkg.RosPack().get_path('neatness_estimator'),
+                'trained_data/sample.csv'))
 
-    def run(self, color, geometry, group):
+        self.classifier = None
+        self.generate_model(self.model_path)
+
+        rospy.Service('~classify', GetMotionPrimitive, self.service_callback)
+
+    def generate_model(self, model_path):
+        self.classifier = RandomForestClassifier(max_depth=2, random_state=0)
+        test_data = [] #x
+        trained_data = [] #y
+        with open(self.model_path) as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            for row in reader:
+                y = float(row[0])
+                x = map(lambda x : float(x), row[1:])
+                test_data.append(x)
+                trained_data.append(y)
+
+        self.classifier.fit(np.array(test_data), np.array(trained_data))
+
+    def run(self, target_data):
+        if self.classifier is None:
+            return None
+
         # random forest
-
-        motion_primitive = 'rotate'
-        return motion_primitive
+        motion_class = self.classifier.predict(target_data)
+        motion_class = int(motion_class[0])
+        return self.motion_lst[motion_class]
 
     def service_callback(self, req):
         res = GetMotionPrimitiveResponse()
 
         motion_primitives = []
-        for color, geo, group in zip(
+        for color, geometry, group in zip(
                 req.color_distance,
                 req.geometry_distance,
                 req.group_distance):
-            motion_primitive = run(color, geo, group)
+            target_data = np.array([color, geometry, group], dtype=np.float64)
+            motion_primitive = self.run(target_data)
+            if motion_primitive is None:
+                rospy.logwarn('failed to motion classification')
+                continue
+
             motion_primitives.append(motion_primitive)
 
         res.motions = motion_primitives
