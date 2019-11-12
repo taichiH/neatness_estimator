fc52513d1d667ad795d2d64e9f1bb4505525f340
taichiH <higashide@jsk.imi.i.u-tokyo.ac.jp>
2019-11-12 14:17:24 +0900

    add motion classifier launch files

--- neatness_estimator/scripts/get_motion_primitive.py
+++ neatness_estimator/scripts/get_motion_primitive.py
@@ -21,6 +21,7 @@ class GetMotionPrimitiveServer():
                 'trained_data/sample.csv'))
 
         self.classifier = None
+        rospy.loginfo('model_path: %s' %(self.model_path))
         self.generate_model(self.model_path)
 
         rospy.Service('~classify', GetMotionPrimitive, self.service_callback)
@@ -29,22 +30,30 @@ class GetMotionPrimitiveServer():
         self.classifier = RandomForestClassifier(max_depth=2, random_state=0)
         test_data = [] #x
         trained_data = [] #y
+        labels = []
         with open(self.model_path) as csvfile:
             reader = csv.reader(csvfile, delimiter=',')
-            for row in reader:
+            for i, row in enumerate(reader):
+                if i == 0:
+                    labels = row
+                    continue
+
                 y = float(row[0])
                 x = map(lambda x : float(x), row[1:])
                 test_data.append(x)
                 trained_data.append(y)
 
+        print(test_data)
+        print(trained_data)
         self.classifier.fit(np.array(test_data), np.array(trained_data))
 
     def run(self, target_data):
         if self.classifier is None:
             return None
 
+        print('target_data', target_data)
         # random forest
-        motion_class = self.classifier.predict(target_data)
+        motion_class = self.classifier.predict(np.array([target_data]))
         motion_class = int(motion_class[0])
         return self.motion_lst[motion_class]
 
@@ -57,6 +66,7 @@ class GetMotionPrimitiveServer():
                 req.geometry_distance,
                 req.group_distance):
             target_data = np.array([color, geometry, group], dtype=np.float64)
+
             motion_primitive = self.run(target_data)
             if motion_primitive is None:
                 rospy.logwarn('failed to motion classification')
@@ -69,6 +79,6 @@ class GetMotionPrimitiveServer():
         return res
 
 if __name__=='__main__':
-    rospy.init_node('get_motion_primitive_service')
+    rospy.init_node('get_motion_primitive_server')
     get_motion_primitive_server = GetMotionPrimitiveServer()
     rospy.spin()
