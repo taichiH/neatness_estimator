b2bf242cfd9263b5e7cdda5656c6da8691e4e439
taichiH <higashide@jsk.imi.i.u-tokyo.ac.jp>
2019-11-12 16:10:47 +0900

    add data_corrector

---
+++ neatness_estimator/scripts/data_corrector.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import os
+import csv
+
+import rospy
+import rospkg
+from neatness_estimator_msgs.srv import CorrectData, CorrectDataResponse
+
+class DataCorrector():
+
+    def __init__(self):
+        self.model_file_path = rospy.get_param(
+            '~model_path',
+            os.path.join(
+                rospkg.RosPack().get_path('neatness_estimator'),
+                'trained_data/sample.csv'))
+
+        rospy.loginfo('model_path: %s' %(self.model_file_path))
+        rospy.Service('~correct', CorrectData, self.service_callback)
+
+    def service_callback(self, req):
+        res = CorrectDataResponse()
+
+        save_data = [req.label] + list(req.data)
+        print('save_data', save_data)
+        with open(self.model_file_path, 'a') as f:
+            writer = csv.writer(f)
+            writer.writerow(save_data)
+
+        res.success = True
+        return res
+
+if __name__=='__main__':
+    rospy.init_node('data_corrector')
+    data_corrector = DataCorrector()
+    rospy.spin()
