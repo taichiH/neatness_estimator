ddecd005bbdc43df32d84bf369af498a369d53ea
taichiH <higashide@jsk.imi.i.u-tokyo.ac.jp>
2019-11-28 19:27:07 +0900

    add difference_estimation_client.py

---
+++ neatness_estimator/scripts/difference_estimation_client.py
@@ -0,0 +1,8 @@
+import rospy
+from neatness_estimator_msgs.srv import GetDifference, GetDifferenceResponse
+
+rospy.init_node('/difference_estimation_client')
+client = rospy.ServiceProxy(
+    '/estimation_module_interface_color_and_geometry/call', GetDifference)
+res = client(task='two_scene')
+print(res)
